8794e10fee358e74c564b088f1b578c88bc4003b
Gyorgy_Suveges <Gyorgy_Suveges@epam.com>
2014-05-26 00:55:59 +0200

    Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We create functions that inverse a matrix in a cached way. 
+## Once we get the matrix we'll store the result, next time
+## we get it we'll return the stored value
 
-## Write a short comment describing this function
+## makeCacheMatrix - creates a matrix wrapper "class"
+## with some caching and setter-getter functions
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -19,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## CacheSolve makes the actual cached computation using the previous function
 
 cacheSolve <- function(x, ...) {
   i <- x$getInverse()
